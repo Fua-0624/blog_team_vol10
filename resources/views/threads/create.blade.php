56344e3dca0e92c1f958f8eace5056d31c3a810c
tado-chan <glacialeafeon@gmail.com>
2023-08-26 17:48:41 +0900

    コメントとスレッド作成画面です

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -1,32 +1,26 @@
 <x-app-layout>
     <x-slot name="header">
-        {{ __('thread')}}
+        {{$game->title}}
     </x-slot>
     <body>
-        <h1>チーム開発会へようこそ！</h1>
-        <h2>投稿作成</h2>
-        <form action="/posts" method="POST">
+      <div style="width:50%; margin: 0 auto; text-align:center;">
+        <form action="/threads/post" method="POST">
             @csrf
             <div>
-                <h2>タイトル</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
-                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+               <h2>タイトル</h2>
+
+    タイトル:<br>
+    <input type="text" name="title" placeholder="スレッドの目的" >
+    <br>
+    コメント:<br>
+    内容:
+    <input type="text" input name="内容" placeholder="説明"　>
+    <br>
+    {{ csrf_field() }}
+    <button class="btn btn-success"> 送信 </button>
+</form>
+
             </div>
-            <div>
-                <h2>本文</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ old('post.body') }}</textarea>
-                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
-            </div>
-            <div>
-                <h2>カテゴリー</h2>
-                <select name="post[category_id]">
-                    @foreach($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <input type="submit" value="保存"/>
-        </form>
-        <div><a href="/">戻る</a></div>
+            
     </body>
 </x-nav-layout>
