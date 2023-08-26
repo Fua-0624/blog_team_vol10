46dec987e1d81e1857b4f8c7599b3848b63004a5
Fua-0624 <fua.sugimura.kochi@gmail.com>
2023-08-26 17:29:40 +0900

    web.phpの編集

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -6,7 +6,8 @@
         <h1>スレッドの概要</h1>
         <p>{{ $thread->body }}</p>
         <h1>コメント</h1>
-        <div>
+        <p class="create">[<a href="/threads/{{ $thread->id }}/comment/create">コメント作成</a>]</p>
+        <div class="flex">
             @foreach ($comments as $comment)
                 <p>{{ $comment->created_at }}</p>
                 <p>{{ $comment->user->name }}</p>
