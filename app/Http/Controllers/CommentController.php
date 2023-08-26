2727b188cb6d4da7657bad820d231183628a0baf
ueda1016 <uedasaki03@gmail.com>
2023-08-26 19:52:49 +0900

    コントローラーの編集

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Game;
+use App\Models\Thread;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function create(Game $game , Thread $thread)
+    {
+        return view('comments.create')->with(['game' => $game , 'thread' => $thread]);
+    }
+    
+    public function store(Request $request , Comment $comment)
+    {
+        $input = $request['comment'];
+        $comment->fill($input)->save();
+        return redirect('/games/' . $comment->thread->game->id . '/threads/' . $comment->thread->id);
+    }
 }
