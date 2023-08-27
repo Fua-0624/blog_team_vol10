732b385315d786fba4949446f4a09c93db64d8b5
tado-chan <glacialeafeon@gmail.com>
2023-08-27 17:34:40 +0900

    kaemashita

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -8,7 +8,7 @@
         @csrf
         <div> 
       <div class="Form-Item">
-    <p class="Form-Item-Label isMsg"><span class="Form-Item-Label-Required">任意</span>コメント内容</p>
+    <p class="Form-Item-Label isMsg"><span class="Form-Item-Label-Required">必須</span>コメント内容</p>
     <textarea class="Form-Item-Textarea"></textarea>
   </div>
   <input type="submit" class="Form-Btn" value="送信する">
